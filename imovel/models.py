bb05f1cbd61b960f932f1b7fee5b493a15032953
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-10-04 01:19:59 -0300

    LOCALIZACAO: add relacao imovel com regiao

--- imovel/models.py
+++ imovel/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from localizacao.models import Regiao
 
 
 class Tipo(models.Model):
-    nome = models.CharField(max_length=30)
+    nome = models.CharField('Nome', max_length=30)
 
     def __str__(self):
         return self.nome
@@ -13,20 +14,33 @@ class Tipo(models.Model):
 
 
 class Propriedade(models.Model):
-    nome = models.CharField(max_length=50)
-    valor = models.DecimalField(decimal_places=2, max_digits=12)
+    nome = models.CharField('Nome', max_length=50)
+    valor = models.DecimalField('Valor', decimal_places=2, max_digits=12)
     tipo = models.ForeignKey(
         Tipo, verbose_name='Tipo',
-        related_name='propriedade',
+        related_name='propriedade_tipo',
         on_delete=models.PROTECT
     )
     foto = models.FileField()
+
+    regiao = models.ForeignKey(
+        Regiao, verbose_name='Região',
+        related_name='propriedade_regiao',
+        on_delete=models.PROTECT,
+        blank=True, null=True
+    )
 
     criado_em = models.DateTimeField('Criado em', auto_now_add=True)
     modificado_em = models.DateTimeField('Modificado em', auto_now=True)
 
     def __str__(self):
-        return self.nome
+        return f'Propriedade: {self.nome}, Tipo: {self.tipo.nome}, Região: {self.get_regiao()}'
+
+    def get_tipo(self):
+        return self.tipo.nome
+
+    def get_regiao(self):
+        return self.regiao.nome
 
     class Meta:
         verbose_name = 'Propriedade'
